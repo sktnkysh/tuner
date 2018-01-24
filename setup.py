209da7bb1c54a10751c7774c0b51710e45174a68
sktnkysh <sktnkysh+dev@gmail.com>
2018-01-24 17:38:07 +0900

    Add scripts

--- setup.py
+++ setup.py
@@ -8,4 +8,8 @@ setup(
     author='FUKUDA Yutaro',
     url='https://github.com/sktnkysh/tuner',
     packages=find_packages(),
+    entry_points={
+        'console_scripts':
+        ['format-dataset=tuner.scripts.format_dataset:main', 'toon=tuner.scripts.toon:main']
+    },
 )
