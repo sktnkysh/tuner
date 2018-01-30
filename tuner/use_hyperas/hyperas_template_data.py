94404f60d9f0315b18bd9c416b94ea8640d5da5f
sktnkysh <sktnkysh+dev@gmail.com>
2018-01-30 14:24:54 +0900

    Modify sampling unbalanced data at AugmentDataset.search_opt_augment

--- tuner/use_hyperas/hyperas_template_data.py
+++ tuner/use_hyperas/hyperas_template_data.py
@@ -4,6 +4,8 @@ def data():
     resize = '{resize}'
     rescale = '{rescale}'
     df = load_data.df_fromdir_classed(train_dir)
+    sampling_size = min(df.label.value_counts()) * 4
+    df = load_data.oversampling_df(df, sampling_size)
     x_train, y_train = load_data.load_fromdf(df, resize=resize, rescale=rescale)
     df = load_data.df_fromdir_classed(test_dir)
     x_test, y_test = load_data.load_fromdf(df, resize=resize, rescale=rescale)
